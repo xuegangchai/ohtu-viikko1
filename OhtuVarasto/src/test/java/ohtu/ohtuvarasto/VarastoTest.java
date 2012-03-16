eae4ff0602768b09a6ab2e94357d35e39dd1d026
Chai Xuegang <xuegang.chai@helsinki.fi>
2012-03-16 17:41:47 +0200

    Varasto Ohjelma

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -35,7 +35,7 @@ public class VarastoTest {
         varasto.lisaaVarastoon(8);
 
         // saldon pitäisi olla sama kun lisätty määrä
-        assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(12, varasto.getSaldo(), vertailuTarkkuus);
     }
     
     @Test
